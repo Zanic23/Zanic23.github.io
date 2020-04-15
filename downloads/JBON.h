aca853a26d4097fd426e7e98b39fc57888a03ce2
Zanic23 <57819528+Zanic23@users.noreply.github.com>
2020-04-15 15:35:43 -0600

    zip header

---
+++ downloads/JBON.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "ClassCollection.h"
+#include "ObjectCollection.h"
+
+#include "GenericClass.h"
+#include "ClassObject.h"
+
+#include "object.h"
+#include "Pair.h"
+#include "Array.h"
+
+#include "value.h"
+#include "ValueType.h"
+
+#include "Parser.h"
+#include "File.h"
+#include "StringFunctions.h"
